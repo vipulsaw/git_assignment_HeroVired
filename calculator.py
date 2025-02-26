b8573a3651958c518cfba9776dfd5ac5d0a1d667
vipulsaw <vipulsawale9@gmail.com>
2025-02-26 23:11:21 +0530

    freature sqrt push

--- calculator.py
+++ calculator.py
@@ -14,8 +14,8 @@ class Calculator:
         return a / b
 
     # TODO: Implement the following function to calculate the square root of a number.
-   # def square_root(self, x):
-    #    return math.sqrt(x)
+    def square_root(self, x):
+        return math.sqrt(x)
 
 if __name__ == "__main__":
     calculator = Calculator()
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
     # TODO: Uncomment and test the square root feature.
-    #num3 = 25
-    #print(f"The square root of {num3} = {calculator.square_root(num3)}")
+    num3 = 25
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
