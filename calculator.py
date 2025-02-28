2d6d5416a802aaea7512ba728f356d25624cadd0
vipulsaw <vipulsawale9@gmail.com>
2025-02-28 19:36:37 +0530

    bug new fixation for cannot divide by zero

--- calculator.py
+++ calculator.py
@@ -12,13 +12,12 @@ class Calculator:
 
     def divide(self, a, b):
         if b == 0:
-        raise ValueError("Cannot divide by zero.")
+         raise ValueError("Cannot divide by zero.")
         return a / b
 
     # TODO: Implement the following function to calculate the square root of a number.
    # def square_root(self, x):
     #    return math.sqrt(x)
-
 if __name__ == "__main__":
     calculator = Calculator()
 
